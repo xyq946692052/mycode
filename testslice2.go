de6b02552fe7de9eac3eacbbf617fb555c6a22cf
xyq <xyq@oilo2o.com>
2014-09-29 16:21:27 +0800

    modified something

--- testslice2.go
+++ testslice2.go
@@ -1,21 +1,22 @@
 package main
 
 import "fmt"
-
 func main(){
   myslice1:=make([]int,5)
   myslice2:=make([]int,5,10)
-  myslice3:=[]int{1,2,3,4,5}
+  myslice:=[]int{1,2,3,4,5}
+  myslice3:=append(myslice,1,2,3)
+  copy(myslice3,myslice1)
   for i:=0;i<len(myslice1);i++{
     fmt.Println("myslice1[",i,"]=",myslice1[i])
   }
   fmt.Println("--------------------")
   for i:=0;i<len(myslice2);i++{
-  fmt.Println("myslice2[",i,"]=",myslice2[i])
+    fmt.Println("myslice2[",i,"]=",myslice2[i])
   }
   fmt.Println("--------------------")
   for i:=0;i<len(myslice3);i++{
-  fmt.Println("myslice3[",i,"]=",myslice3[i])
+    fmt.Println("myslice3[",i,"]=",myslice3[i])
   }
   fmt.Println("--------------------")
   fmt.Println("myslice1's long :",len(myslice1),",space is ",cap(myslice1))
